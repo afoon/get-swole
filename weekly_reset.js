9a2056877c310eb37fa3f4a99bdb3559f96ffd8b
Amina <amefoon@gmail.com>
2021-05-02 23:50:35 -0400

    clean it up

--- weekly_reset.js
+++ weekly_reset.js
@@ -1,13 +1,5 @@
-const Player = require("./db").Player;
-const { createLeaderBoard, sendLeaderBoard } = require("./helpers");
+const { createLeaderBoard, sendLeaderBoard, clearTheStats } = require("./helpers");
 const { DateTime } = require("luxon");
-
-const clearTheStats = () => {
-  const reset = { workouts: 0, meals: 0, challenge: false, totalPoints: 0 };
-  Player.updateMany({}, reset, { multi: true }, (err) => {
-    if (err) throw err;
-  });
-};
 
 const resetText = `And the week is over! \n Here are the end results`
 const weeklyReset = async () => {
